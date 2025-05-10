f65a806e1397757e0731417aab46826ce57e6d14
Kaliboy001 <mr.shokrullah@gmail.com>
2025-05-10 10:56:35 +0430

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,12 +3,12 @@
 
 class BotConfig:
     SESSION_NAME = "YoutubeGaelanSession"
-    API_ID = 21211216
-    API_HASH = "7404d8663463075dfd564027f32810fc"
-    BOT_TOKEN = "6019956632:AAFHRc_PRuD8FFTfDBP4AJefb0zqnabOJt4"
-    BOT_USERNAME = "youtube_gaelan_bot"  # Without @, example Folabetbot
-    DEFULT_ADMINS_USER_ID = {5283609058, }
-    CREATOR_USER_ID = 5283609058
+    API_ID = 15787995
+    API_HASH = "e51a3154d2e0c45e5ed70251d68382de"
+    BOT_TOKEN = "7929166887:AAHsCIDiSVTGdoh9fHy9Y41U3PopgFI3imo"
+    BOT_USERNAME = "whajaiaiabot"  # Without @, example Folabetbot
+    DEFULT_ADMINS_USER_ID = {7046488481, }
+    CREATOR_USER_ID = 7046488481
     SUPPORT_USERNAME = "Jarrare" # without @, example Jarrae
     REFERRAL_IMAGE_ADDRESS = r"./images/referral.png"
     DEFULT_SUPPORT_CHANNEL_URL = "t.me/Gae1an" # url form, not username
